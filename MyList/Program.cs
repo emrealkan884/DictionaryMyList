0646841866f095396e6a98bd941c332973c33e23
emrealkan884 <Hasan.alkan884@gmail.com>
2021-01-22 23:23:47 +0300

    Add project files.

---
+++ MyList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Emre Alkan");
+            Console.WriteLine(names.Count); 
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] myArray;
+        T[] tempArray;
+        public MyList()
+        {
+            myArray = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempArray = myArray;
+            myArray = new T[myArray.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                myArray[i] = tempArray[i];
+            }
+            myArray[myArray.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return myArray.Length; }
+        }
+    }
+}
